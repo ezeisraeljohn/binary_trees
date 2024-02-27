6210fdef0c2007527ba3ba901071bb32ee17e486
Eze Israel John <ezeisrael499@gmail.com>
2024-02-27 13:15:04 +0100

    Returns NULL if parent is NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,7 +22,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	if (!parent)
-		return (new_node);
+		return (NULL);
 
 	/**
 	 * make @new_node, left child of parent node and old child node, the
