eddaa33b6d29f22f23449005c56311a821801c10
Eze Israel John <ezeisrael499@gmail.com>
2024-02-27 10:50:34 +0100

    Removed the comment

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,8 +17,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 
 	/* New nodes dosen't have child so set right and left to null */
-	/*new_node->left = NULL;*/
-	/*new_node->right = NULL;*/
+	new_node->left = NULL;
+	new_node->right = NULL;
 
 	if (parent != NULL)
 	{
