d790e1ffd5e1bd0d62c700af58a696510a3dde49
Eze Israel John <ezeisrael499@gmail.com>
2024-02-27 22:11:40 +0100

    Makes function comform to betty code style

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,12 +3,12 @@
 /**
  * binary_tree_is_root - This checks if a node is the root node of a tree
  * @node: The node to check
- * 
+ *
  * Return: 1 if node is the root node else zero
-*/
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (!node)
+	if (!node)
 		return (0);
 
 	if (node->parent == NULL)
