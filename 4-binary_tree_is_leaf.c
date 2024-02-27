9f93a10ca13136f24da4a853dd76e15d74cc2b7e
Eze Israel John <ezeisrael499@gmail.com>
2024-02-27 22:02:45 +0100

    Resolved the issue of documentation and betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,16 +3,16 @@
 /**
  * binary_tree_is_leaf - Checks if a node is a leaf node
  * @node: The node to be checked
- * 
- * Return 1 if the node is a leaf node or zero 
-*/
+ *
+ * Return: Return 1 if the node is a leaf node or zero otherwise
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (!node)
-        return (0);
+	if (!node)
+		return (0);
 
-    if ((node->left == NULL) && (node->right == NULL))
-        return (1);
-    else
-        return (0);
+	if ((node->left == NULL) && (node->right == NULL))
+		return (1);
+	else
+		return (0);
 }
